a83160431dd7461d6bb437892275169d67021ae8
Jchan <gkswocks123@gmail.com>
2022-08-23 18:59:00 +0900

    Feat : Install  react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// Pages
+import Home from "./pages/Home";
+import Fail from "./pages/Fail";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route path="/home" element={<Home />} />
+                    <Route path="*" element={<Fail />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/Fail/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// [Todo] http status 오류 메세지를 담음
+function Fail() {
+    return <div>Fail</div>;
+}
+
+export default Fail;
