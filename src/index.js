f00a9969f83e052602c2611d5c01413d0d4f8199
Jchan <gkswocks123@gmail.com>
2022-08-23 19:16:13 +0900

    Feat : Install react-query

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import "./index.css";
 import App from "./App";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            // Global option
+            staleTime: 60 * 1000, // [Ref] Just initial setting
+            refetchOnWindowFocus: false,
+        },
+    },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
     </React.StrictMode>
 );
