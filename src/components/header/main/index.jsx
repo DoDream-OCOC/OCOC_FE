f3103351d33297ebe350ebcbd65b038e7265ddb0
Jchan <gkswocks123@gmail.com>
2022-08-27 18:10:03 +0900

    Feat : MainHeader UI 완성

--- src/components/header/main/index.jsx
+++ src/components/header/main/index.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 import style from './index.module.css';
 import { ReactComponent as Logo } from '../../../assets/fake/logo.svg';
+import { Text } from '../../element';
 
 function MainHeader() {
   return (
     <header>
-      <Logo />
+      <div className={style.flex}>
+        <Logo style={{ marginRight: '0.5rem' }} />
+        <Text size="M-bold" text={'Logo here'} />
+      </div>
+      <div>hamburger</div>
     </header>
   );
 }
