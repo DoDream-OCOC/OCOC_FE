7722798b04790ffde25e6a0cb844ee18f88e1a1d
Jchan <gkswocks123@gmail.com>
2022-08-29 20:58:59 +0900

    Comment : Add toTo

--- src/pages/LevelSelection/index.jsx
+++ src/pages/LevelSelection/index.jsx
@@ -15,6 +15,7 @@ function LevelSelection() {
             <Text size="M-Bold" content="학습 레벨을 선택해주세요." style={{ textAlign: 'left', marginLeft: '0.5rem' }} />
           </div>
           <Empty size="3.2rem" />
+          {/* [Todo] 48rem 이후 웹 CSS 만들기 */}
           <LevelSelectionBtn title="초급" content="3~5개 단어 클릭 영작" />
           <LevelSelectionBtn title="중급" content="6~10개 단어 클릭 영작" />
           <LevelSelectionBtn title="고급" content="11~15개 단어 클릭 영작" />
