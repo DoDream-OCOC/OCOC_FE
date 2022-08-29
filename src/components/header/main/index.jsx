263c96c3f0c9fdf25c6b907672d0bcc709fcc5f9
Jchan <gkswocks123@gmail.com>
2022-08-29 19:59:14 +0900

    Feat : Add naviagte

--- src/components/header/main/index.jsx
+++ src/components/header/main/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import style from './index.module.css';
 import { ReactComponent as Logo } from '../../../assets/fake/logo.svg';
 import { Text } from '../../element';
 
 function MainHeader() {
+  const navigate = useNavigate();
+
   return (
     <header>
-      <div className={style.flex}>
+      <div className={style.flex} onClick={() => navigate('/')}>
         <Logo title="header-logo" style={{ marginRight: '0.5rem' }} />
         <Text size="M" content={'Logo here'} />
       </div>
