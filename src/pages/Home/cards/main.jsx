ae8f64ec1950e5d21de5ae5e324069164526a12f
Jchan <gkswocks123@gmail.com>
2022-08-29 20:52:14 +0900

    Feat : Create LevelSelection page

--- src/pages/Home/cards/main.jsx
+++ src/pages/Home/cards/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { Text, Empty, Button } from '../../../components/element';
 import style from './index.module.css';
 
-// [Temp] 그냥 이름 생각안나서 card
 export const MainCard = () => {
+  const navigate = useNavigate();
+
   return (
     <article>
       <Text size="M" content="영어 작문의 꾸준한 첫걸음," />
@@ -12,7 +14,7 @@ export const MainCard = () => {
       {/* [Todo] 시멘틱웹을 고려해 h태그로 고치기 */}
       <Text size="L" content="영차영차" />
       <Empty size="5rem" />
-      <Button />
+      <Button onClick={() => navigate('/lvl-slc')} />
     </article>
   );
 };
--- src/pages/LevelSelection/LevelSelection.test.jsx
+++ src/pages/LevelSelection/LevelSelection.test.jsx
@@ -24,5 +24,11 @@ describe('LevelSelection page', () => {
     screen.getByTitle('header-hamburger');
 
     test.text('학습 레벨을 선택해주세요.');
+    test.text('초급');
+    test.text('3~5개 단어 클릭 영작');
+    test.text('중급');
+    test.text('6~10개 단어 클릭 영작');
+    test.text('고급');
+    test.text('11~15개 단어 클릭 영작');
   });
 });
