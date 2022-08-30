5e55ffa9c19472f3d9667e2bf62cf77c57bbc7e0
Jchan <gkswocks123@gmail.com>
2022-08-30 23:00:03 +0900

    Feat : Set initial MSW settings

--- src/index.js
+++ src/index.js
@@ -14,6 +14,11 @@ const queryClient = new QueryClient({
   },
 });
 
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser');
+  worker.start();
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
