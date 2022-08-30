b9e595105a88069cb0d747c0fa115186177b0d62
kje <duji1207@gmail.com>
2022-08-30 16:56:34 +0900

    Feat : Create Hamburger(Navigation) component

---
+++ src/components/header/main/Hamburger.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { Text } from '../../element';
+import style from './index.module.css';
+import { FaBars, FaBeer, FaTimes } from 'react-icons/fa';
+
+function Hamburger() {
+
+    const [isMobile, setIsMobile] = useState(false);
+
+    return(
+        <nav className={style.navbar}>
+            <Text className={style.logo} size="M" text={'Logo here'} />
+            <ul className={isMobile ? style.nav_links_mobile : style.nav_links}
+            onClick={() => setIsMobile(false)}>
+                <li className={style.premium}><Text size="S" text={'프리미엄 서비스'} /></li>
+                <li className={style.tip}><Text size="S" text={'작문 팁'} /></li>
+                <li className={style.debate}><Text size="S" text={'영작 팁'} /></li>
+                <li className={style.sign_up}><Text size="S" text={'Sign up'} /></li>
+            </ul>
+            <button className={style.mobile_menu_icon}
+            onClick={() => setIsMobile(!isMobile)}>
+                {isMobile ? (
+                    <FaTimes />
+                ) : (
+                    <FaBars />
+                )}
+            </button>    
+        </nav>
+    );
+}
+
+export default Hamburger;
--- src/components/header/main/index.jsx
+++ src/components/header/main/index.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import style from './index.module.css';
 import { ReactComponent as Logo } from '../../../assets/fake/logo.svg';
 import { Text } from '../../element';
+import Hamburger from './Hamburger';
+
 
 function MainHeader() {
+  const navigate = useNavigate();
+
   return (
+    <>
     <header>
-      <div className={style.flex}>
-        <Logo style={{ marginRight: '0.5rem' }} />
-        <Text size="M" text={'Logo here'} />
+      <div className={style.flex} onClick={() => navigate('/')}>
+        <div title="header-hamburger"><Hamburger /></div>
       </div>
-      <div>hamburger</div>
     </header>
+    </>
   );
 }
 
