8d77306234b3ccd0a7a32bbf4582ac1bcab50d6f
jungeol <dali2728@gmail.com>
2022-09-03 20:12:08 +0900

    폰트 스타일 재지정, 적용 완료 아이콘 svg 일부 업로드

--- src/components/header/main/index.jsx
+++ src/components/header/main/index.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 
 import style from './index.module.css';
 import { ReactComponent as Logo } from '../../../assets/fake/logo.svg';
+import { ReactComponent as OCOC_title} from '../../../assets/OCOC/OCOC_text.svg';
+import { ReactComponent as Profile} from '../../../assets/icons/icon_profile.svg';
 import { Text } from '../../element';
 
+// todo 로고랑 타이틀 누르면 메인페이지로 이동
 function MainHeader() {
   return (
     <header>
       <div className={style.flex}>
         <Logo style={{ marginRight: '0.5rem' }} />
-        <Text size="M" text={'Logo here'} />
+        <OCOC_title style={{ width: '3.87rem', height: '1.06rem' , fill:'var(--Black)'}} />
       </div>
-      <div>hamburger</div>
+      <div>
+        <Profile />
+      </div>
     </header>
   );
 }
