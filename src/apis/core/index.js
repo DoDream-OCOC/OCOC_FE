e59a348eddf75adb7f69a4ed3b9cdcce1d3a7531
Jchan <gkswocks123@gmail.com>
2022-09-06 22:41:59 +0900

    Feat : Axios 인터셉터 수정

--- src/apis/core/index.js
+++ src/apis/core/index.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// [Temp] API개발완료되면 적용
-const BASE_URL = null; // process.env.REACT_APP_BASE_URL
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const ococ = axios.create({
   headers: { Accept: 'application/json' },
--- src/apis/core/intercepter.js
+++ src/apis/core/intercepter.js
@@ -1,13 +1,43 @@
 import ococ from '.';
 
+// [Temp]
+const TOKEN_EXPIRED = 499;
+
 ococ.interceptors.request.use(
-  config => config,
+  config => {
+    // [Temp] 토큰을 담아서 보내지 않을 때
+    if (config.url === '') {
+      return config;
+    } else {
+      // return {
+      //   ...config,
+      //   headers: {
+      //     // [Temp] 따로 전역관리 안하면 그냥 로컬에서/세션에서 가져옴
+      //     Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+      //   },
+      // };
+    }
+  },
   error => error,
 );
 
 ococ.interceptors.response.use(
   response => response,
   async error => {
+    const {
+      config,
+      response: { status, data },
+    } = error;
+
+    console.log('URL:', config.url);
+    console.log('Status:', status);
+    console.log('Authorization:', config.headers.authorization);
+    console.log('msg:', data.message);
+
+    if (status === TOKEN_EXPIRED) {
+      // [Todo] 토큰 재발급 처리
+    }
+
     return Promise.reject(error);
   },
 );
