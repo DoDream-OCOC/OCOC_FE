7c5bf11d22d57057b3f01bdeb7c1247cce2dbe93
Jchan <gkswocks123@gmail.com>
2022-09-07 16:12:48 +0900

    Remove: Delete css

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,16 +5,14 @@ import MainContainer from '../../components/container/main';
 import MainFooter from '../../components/footer/main';
 import { MainCard, FeatureCard } from './cards';
 
-import style from './index.module.css';
-
 function Home() {
   return (
     <>
       <Navbar />
       <MainContainer>
         <MainCard />
-        <FeatureCard  type="left" title="영작, 클릭으로 쉽게" content="순서대로 단어를 클릭해 영어 작문을 연습하세요!"/>
-        <FeatureCard type="right" title="PC, 모바일 어디에서든" content="장소에 구애받지 않고 어디서든 학습하세요"/>
+        <FeatureCard type="left" title="영작, 클릭으로 쉽게" content="순서대로 단어를 클릭해 영어 작문을 연습하세요!" />
+        <FeatureCard type="right" title="PC, 모바일 어디에서든" content="장소에 구애받지 않고 어디서든 학습하세요" />
       </MainContainer>
       <MainFooter />
     </>
