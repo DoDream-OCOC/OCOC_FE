8fc620e397bcf9e87f7c5d2967b1a2899241c5ac
kje <duji1207@gmail.com>
2022-09-07 18:01:28 +0900

    Feat : Create ClickEng page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Fail from './pages/Fail';
 import LevelSelection from './pages/LevelSelection';
+import ClickEng from './pages/ClickEng';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/lvl-slc" element={<LevelSelection />} />
+          <Route path="/click-eng" element={<ClickEng />} />
           <Route path="*" element={<Fail />} />
         </Routes>
       </Router>
