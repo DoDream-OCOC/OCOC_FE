d1a38bdbddc968d28ef6bc340fb64ade2ee26f87
Jchan <gkswocks123@gmail.com>
2022-09-10 20:34:06 +0900

    Style : Add 768px~ style of levelSelection page

--- src/pages/LevelSelection/index.jsx
+++ src/pages/LevelSelection/index.jsx
@@ -8,6 +8,8 @@ import MainContainer from '../../components/container/main';
 import { LevelSelectionBtn } from './Button';
 import { Empty, Text } from '../../components/element';
 
+import styled from './index.module.css';
+
 function LevelSelection() {
   const mutation = useMutation({
     mutationFn: data => study.sendStudyType(data),
@@ -29,10 +31,11 @@ function LevelSelection() {
             <Text size="H4" content="학습 레벨을 선택해주세요." style={{ textAlign: 'left', marginLeft: '0.5rem' }} />
           </div>
           <Empty size="3.2rem" />
-          {/* [Todo] 48rem 이후 웹 CSS 만들기 */}
-          <LevelSelectionBtn onClick={onClick} title="초급" content="3~5개 단어 클릭 영작" />
-          <LevelSelectionBtn onClick={onClick} title="중급" content="6~10개 단어 클릭 영작" />
-          <LevelSelectionBtn onClick={onClick} title="고급" content="11~15개 단어 클릭 영작" />
+          <div className={styled.flexDirection}>
+            <LevelSelectionBtn onClick={onClick} title="초급" content="3~5개 단어 클릭 영작" />
+            <LevelSelectionBtn onClick={onClick} title="중급" content="6~10개 단어 클릭 영작" />
+            <LevelSelectionBtn onClick={onClick} title="고급" content="11~15개 단어 클릭 영작" />
+          </div>
           <Empty size="3.2rem" />
         </article>
       </MainContainer>
