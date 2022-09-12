152e393ef3403bde35b783ade7b7d8de2c6c6f66
Jchan <gkswocks123@gmail.com>
2022-09-12 10:26:54 +0900

    Feat : Add shiftCorpus hook

---
+++ src/hooks/useShiftCorpus/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+function useShiftCorpus() {
+  const aCorpus = useSelector(state => state.study.words.shift());
+  return aCorpus;
+}
+
+export default useShiftCorpus;
