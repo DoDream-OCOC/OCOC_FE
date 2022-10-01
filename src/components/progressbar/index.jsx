cb86f35b70f234c419dbb6ef8a3a1ae2587cd69a
Jchan <gkswocks123@gmail.com>
2022-10-01 21:04:02 +0900

    Feat : Add Progressbar's props

--- src/components/progressbar/index.jsx
+++ src/components/progressbar/index.jsx
@@ -2,22 +2,18 @@ import React from 'react';
 import style from './index.module.css';
 import { FaRegTimesCircle } from 'react-icons/fa';
 
-function ProgressBar() {
-    return(
-        <>
-        <nav className={style.progress_navbar}>
-            <button className={style.progress_icon}>
-                <FaRegTimesCircle />
-            </button>
+function ProgressBar(value) {
+  return (
+    <>
+      <nav className={style.progress_navbar}>
+        <button className={style.progress_icon}>
+          <FaRegTimesCircle />
+        </button>
 
-            <progress
-                className={style.progress} 
-                value="5" 
-                max="10"> 
-            </progress>
-        </nav>    
-        </>
-    );
+        <progress className={style.progress} value={value} max="10"></progress>
+      </nav>
+    </>
+  );
 }
 
 export default ProgressBar;
