bb164c580cc67e15366b41b0ecc684d90743fdd2
Jchan <gkswocks123@gmail.com>
2022-10-11 20:27:03 +0900

    Feat : Create default page

---
+++ src/pages/sign/SignIn/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import NavBar from '../../../components/navbar';
+import MainContainer from '../../../components/container/main';
+import { Text, Button } from '../../../components/element';
+
+function SignIn() {
+  return (
+    <>
+      <NavBar />
+      <MainContainer>
+        <article></article>
+      </MainContainer>
+    </>
+  );
+}
+
+export default SignIn;
