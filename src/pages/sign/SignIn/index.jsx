b58005af5f6e0d147a3300998443abf99a571380
Jchan <gkswocks123@gmail.com>
2022-10-15 20:09:21 +0900

    Feat : Add string const variable

--- src/pages/sign/SignIn/index.jsx
+++ src/pages/sign/SignIn/index.jsx
@@ -5,6 +5,8 @@ import NavBar from '../../../components/navbar';
 import MainContainer from '../../../components/container/main';
 import { Text, Button, Empty, Input } from '../../../components/element';
 
+const SIGN_IN = 'sign-in';
+
 function SignIn() {
   const { navigate, reg, onSubmit } = useSignIn();
 
@@ -15,13 +17,13 @@ function SignIn() {
         <article>
           <Text size="H4" color="Text-2" content="로그인" />
           <Empty size="2.5rem" />
-          <form id="signIn" onSubmit={onSubmit}>
+          <form id={SIGN_IN} onSubmit={onSubmit}>
             <Input {...reg.email} type="email" placeholder="Email" />
             <Empty size="1.5rem" />
             <Input {...reg.password} type="password" placeholder="비밀번호" />
           </form>
           <Empty size="3rem" />
-          <Button content="로그인" type="submit" form="signIn" />
+          <Button content="로그인" type="submit" form={SIGN_IN} />
           <Empty size="1.5rem" />
           <Button content="회원가입" onClick={() => navigate('/sign-up')} />
         </article>
