e47b919c94436265a04eeba52a72714cc61bc113
Jchan <gkswocks123@gmail.com>
2022-10-15 19:46:38 +0900

    Feat : Create useSignIn

--- src/pages/sign/SignIn/index.jsx
+++ src/pages/sign/SignIn/index.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
+import { useSignIn } from './useSignIn';
 
 import NavBar from '../../../components/navbar';
 import MainContainer from '../../../components/container/main';
 import { Text, Button, Empty, Input } from '../../../components/element';
 
 function SignIn() {
-  const navigate = useNavigate();
-  const { register, handleSubmit } = useForm({ defaultValues: { email: '', password: '' } });
+  const { navigate, reg, onSubmit } = useSignIn();
 
   return (
     <>
@@ -17,13 +15,13 @@ function SignIn() {
         <article>
           <Text size="H4" color="Text-2" content="로그인" />
           <Empty size="2.5rem" />
-          <form>
-            <Input placeholder="Email" />
+          <form id="signIn" onSubmit={onSubmit}>
+            <Input {...reg.email} type="email" placeholder="Email" />
             <Empty size="1.5rem" />
-            <Input placeholder="비밀번호" />
+            <Input {...reg.password} type="password" placeholder="비밀번호" />
           </form>
           <Empty size="3rem" />
-          <Button content="로그인" />
+          <Button content="로그인" type="submit" form="signIn" />
           <Empty size="1.5rem" />
           <Button content="회원가입" onClick={() => navigate('/sign-up')} />
         </article>
---
+++ src/pages/sign/SignIn/useSignIn.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useMutation } from 'react-query';
+import { useForm } from 'react-hook-form';
+import { isEmail, isRequired } from '../../../utils/validation';
+
+export const useSignIn = () => {
+  const navigate = useNavigate();
+  const mutaion = useMutation();
+  const { register, handleSubmit } = useForm({ defaultValues: { email: '', password: '' } });
+
+  const onSubmit = handleSubmit(async data => {
+    mutaion.mutate();
+  });
+
+  const reg = {
+    email: { ...register('email', { validate: { isEmail } }) },
+    password: { ...register('password', { validate: { isRequired } }) },
+  };
+
+  return { navigate, reg, onSubmit };
+};
