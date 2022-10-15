37277db32082fd25a86dcb96289dd6759f37ba64
Jchan <gkswocks123@gmail.com>
2022-10-15 19:45:01 +0900

    Comment : Usually used for validation.

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,3 +1,4 @@
+// Usually used for validation.
 const email = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
 const engAndNum = /(?=.*[A-Za-z])(?=.*\d)/;
 const specialCharacters = /(?=.*[$@$!%*#?&])/;
