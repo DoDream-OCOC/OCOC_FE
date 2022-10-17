1dcf2807fb512459252b12b2043a06681332899b
Jchan <gkswocks123@gmail.com>
2022-10-17 09:38:06 +0900

    Feat : error message 추가

--- src/pages/sign/SignUp/useSignUp.js
+++ src/pages/sign/SignUp/useSignUp.js
@@ -16,7 +16,9 @@ export const useSignUp = () => {
         isMinLength13: val => isMinLength(val, 13) || '8글자 이상이어야 합니다.',
       },
     }),
-    confirmPassword: register('confirmPassword', { validate: { isRequired: '비밀번호를 재입력해야 합니다.', isSame: val => val === getValues('password') || '비밀번호와 일치하지 않습니다.' } }),
+    confirmPassword: register('confirmPassword', {
+      validate: { isRequired: val => isRequired(val) || '비밀번호를 재입력해야 합니다.', isSame: val => val === getValues('password') || '비밀번호와 일치하지 않습니다.' },
+    }),
   };
 
   const onSubmit = handleSubmit(async data => {
