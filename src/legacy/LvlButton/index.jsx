2cab20a50ea0d9ecfcc67563871432acae2e1cbc
Jchan <gkswocks123@gmail.com>
2022-10-30 18:20:05 +0900

    Feat : Add legacy folder

---
+++ src/legacy/LvlButton/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { Text } from '../../../components/element';
+
+import { ReactComponent as Loading } from '../../../assets/loading/ellipsis.svg';
+import style from './index.module.css';
+
+export const LevelSelectionBtn = ({ title, isLoading = false, content, onClick, SvgImg }) => {
+  const [clicked, setClicked] = React.useState(false);
+
+  const onClicked = () => {
+    onClick((title === '초급' && 1) || (title === '중급' && 2) || (title === '고급' && 3));
+    setClicked(true);
+  };
+
+  return (
+    <div className={style.outer} onClick={onClicked}>
+      {isLoading && clicked ? (
+        <Loading />
+      ) : (
+        <span className={style.inner}>
+          <SvgImg id={style.circle} />
+          <span className={style.text}>
+            <Text size="B2" content={title} />
+            <Text size="B1" content={content} />
+          </span>
+        </span>
+      )}
+    </div>
+  );
+};
