7d1d1fd79f7623aa11248d0ade0a6dc3aa7e4da4
jungeol-h <dali2728@gmail.com>
2022-11-05 01:20:35 +0900

    [design] 배경 gradient 적용

--- src/pages/Home/cards/Main.jsx
+++ src/pages/Home/cards/Main.jsx
@@ -13,17 +13,19 @@ export const MainCard = () => {
     <article>
       <Empty size="1rem" />
       <div style={{ position: 'absolute', x: '400px' }}>
-        <div className={style.video_wrapper}>
-          <video src={header_video} autoPlay muted loop preload="auto"></video>
-        </div>
+        <div className={style.video_wrapper}></div>
       </div>
       <Empty size="17rem" />
       <Text size="H5" color="black" content="영어 작문의 꾸준한 첫걸음," />
       <Empty size="1rem" />
       <OCOCTitle style={{ width: '9.43rem', height: '2.563rem', fill: 'var(--Green)', ZIndex: '3' }} />
       <Empty size="4.9rem" />
-      <Button onClick={() => navigate('/lvl-slc')} content="시작하기" />
+      <Button onClick={() => navigate('')} content="시작하기" />
       <Empty size="6rem" />
     </article>
   );
 };
+
+{
+  /* <video src={header_video} autoPlay muted loop preload="auto"></video> */
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -11,11 +11,15 @@ function Home() {
       <Navbar />
       <MainContainer>
         <MainCard />
-        <FeatureCard type="left" title="영작, 클릭으로 쉽게" content="순서대로 단어를 클릭해 영어 작문을 연습하세요!" />
-        <FeatureCard type="right" title="PC, 모바일 어디에서든" content="장소에 구애받지 않고 어디서든 학습하세요" />
       </MainContainer>
     </>
   );
 }
 
 export default Home;
+
+// FeatureCard depacrated
+{
+  /* <FeatureCard type="left" title="영작, 클릭으로 쉽게" content="순서대로 단어를 클릭해 영어 작문을 연습하세요!" />
+<FeatureCard type="right" title="PC, 모바일 어디에서든" content="장소에 구애받지 않고 어디서든 학습하세요" /> */
+}
