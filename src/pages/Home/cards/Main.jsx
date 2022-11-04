436f1e3e262447182d80bdd258f8f8ae1bbd9f8e
jungeol-h <dali2728@gmail.com>
2022-11-05 02:44:44 +0900

    [design] 랜딩페이지 거북이 로티 버튼 위 정렬

--- src/pages/Home/cards/Main.jsx
+++ src/pages/Home/cards/Main.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 
 import { Text, Empty, Button } from '../../../components/element';
 import { ReactComponent as OCOCTitle } from '../../../assets/OCOC/OCOC_text.svg';
+import { Turtle } from './turtle';
 import style from './index.module.css';
 
 import Lottie from 'lottie-react';
@@ -10,7 +11,6 @@ import cloud_bg from '../../../assets/OCOC/Cloud_background.json';
 
 export const MainCard = () => {
   const navigate = useNavigate();
-
   return (
     <article>
       <Empty size="1rem" />
@@ -27,6 +27,7 @@ export const MainCard = () => {
       </div>
 
       <Empty size="15rem" />
+      <Turtle />
       <Button onClick={() => navigate('')} content="시작하기" />
       <Empty size="6rem" />
     </article>
