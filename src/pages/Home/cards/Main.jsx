90d4a39f23719693437fd4d6413f2588bc708441
jungeol-h <dali2728@gmail.com>
2022-11-05 01:57:29 +0900

    [design] 텍스트 좌측정렬 및 구름 로티 적용

--- src/pages/Home/cards/Main.jsx
+++ src/pages/Home/cards/Main.jsx
@@ -3,8 +3,10 @@ import { useNavigate } from 'react-router-dom';
 
 import { Text, Empty, Button } from '../../../components/element';
 import { ReactComponent as OCOCTitle } from '../../../assets/OCOC/OCOC_text.svg';
-import header_video from '../../../assets/OCOC/ococ_header_video.mp4';
 import style from './index.module.css';
+
+import Lottie from 'lottie-react';
+import cloud_bg from '../../../assets/OCOC/Cloud_background.json';
 
 export const MainCard = () => {
   const navigate = useNavigate();
@@ -13,13 +15,18 @@ export const MainCard = () => {
     <article>
       <Empty size="1rem" />
       <div style={{ position: 'absolute', x: '400px' }}>
-        <div className={style.video_wrapper}></div>
+        <div className={style.video_wrapper}>
+          <Lottie animationData={cloud_bg} />
+        </div>
       </div>
-      <Empty size="17rem" />
-      <Text size="H5" color="black" content="영어 작문의 꾸준한 첫걸음," />
       <Empty size="1rem" />
-      <OCOCTitle style={{ width: '9.43rem', height: '2.563rem', fill: 'var(--Green)', ZIndex: '3' }} />
-      <Empty size="4.9rem" />
+      <div className={style.text_wrapper}>
+        <Text size="H4" color="black" content="영어 순서 맞추기 게임," />
+        <Empty size="0.6rem" />
+        <OCOCTitle style={{ width: '9.43rem', height: '2.563rem', fill: 'var(--Green)', ZIndex: '3' }} />
+      </div>
+
+      <Empty size="15rem" />
       <Button onClick={() => navigate('')} content="시작하기" />
       <Empty size="6rem" />
     </article>
