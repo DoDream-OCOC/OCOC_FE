78ce1ce999600ccb7553413172c3a8e6844cfadb
jungeol-h <dali2728@gmail.com>
2022-11-05 22:27:29 +0900

    bugfix 배경화면 width 여백 수정

---
+++ src/pages/Mypage/cards/Ranking.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, Empty, Button } from '../../../components/element';
+import style from './index.module.css';
+
+export const Ranking = () => {
+  return (
+    <div>
+      <p>test</p>
+    </div>
+  );
+};
