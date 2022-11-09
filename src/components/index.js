6e585c187f13cbb6172ef6f4a6feb10377d53be9
kje <duji1207@gmail.com>
2022-11-09 23:30:04 +0900

    Refactor : components에 ClickPage와 BlankPage UI 리팩터링

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,8 @@ import MainContainer from './container/main';
 import NavBar from './navbar';
 import ProgressBar1 from './progressbar';
 import QuestionContainer from './questionContainer';
+import ButtonContainer from './buttonContainer';
+import BlankPage from './BlankPage';
+import ClickPage from './ClickPage';
 
-export { MainContainer, NavBar, ProgressBar1, QuestionContainer };
+export { MainContainer, NavBar, ProgressBar1, QuestionContainer, ButtonContainer, BlankPage, ClickPage };
--- src/pages/PlayGame/index.jsx
+++ src/pages/PlayGame/index.jsx
@@ -4,29 +4,19 @@ import { useDispatch, useSelector } from 'react-redux';
 import { gameSlice } from '../../store/slices';
 import { useKeywords } from '../../hooks';
 
-import { NavBar, ProgressBar1, MainContainer, QuestionContainer } from '../../components';
+import { NavBar, MainContainer, ClickPage, BlankPage } from '../../components';
 import { Empty, GradingButton } from '../../components/element';
-import Button from './buttons/Button';
-import style from './index.module.css';
 
 function PlayGame() {
   const location = useLocation();
   const dispatch = useDispatch();
-  const { korean } = useSelector(state => state.game.datasets[state.game.stage - 1]);
   const { stage, life } = useSelector(state => state.game);
-
-  const { keywords, newKeywords, setKeywords, createKeywordsId, insertButton, removeButton, onNextStage, handleGameOver, isGrading, isCrtAns, TimerUI, PointEarnedUI, ShowModal, LifeState } =
-    useKeywords();
+  const { keywords, onNextStage, handleGameOver, isGrading, PointEarnedUI, ShowModal } = useKeywords();
 
   React.useLayoutEffect(() => {
     if (life <= 0 || stage === 31) handleGameOver();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [life]);
-
-  React.useLayoutEffect(() => {
-    setKeywords(() => createKeywordsId());
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [stage]);
 
   const initialRender = React.useRef(true);
   React.useEffect(() => {
@@ -43,22 +33,7 @@ function PlayGame() {
       <ShowModal />
       <MainContainer>
         <article>
-          <div className={style.container}>
-            <ProgressBar1 value={(stage - 1) * 10} />
-            <TimerUI />
-            <div className={style.relative}>
-              <QuestionContainer content={korean} />
-              <div className={style.absolute}>
-                <Button isCorrect={isCrtAns} keywords={newKeywords} onClick={removeButton} />
-              </div>
-            </div>
-            <LifeState />
-            <div className={style.button_keyword_container}>
-              <div className={style.button_default_container}>
-                <Button isCorrect={isCrtAns} keywords={keywords} onClick={insertButton} />
-              </div>
-            </div>
-          </div>
+          <BlankPage />
           <PointEarnedUI />
           <GradingButton content="정답 확인하기" isDisabled={keywords.length > 0 || isGrading} onClick={life >= 1 ? onNextStage : null} />
           <Empty size="1rem" />
