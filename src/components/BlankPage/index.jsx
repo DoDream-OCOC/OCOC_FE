6c136776e43d88ac3fee8ea6f5a8285d0de3f367
kje <duji1207@gmail.com>
2022-11-11 01:47:59 +0900

    Feat : 빈칸 영작 UI

--- src/components/BlankPage/index.jsx
+++ src/components/BlankPage/index.jsx
@@ -1,27 +1,43 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { gameSlice } from '../../store/slices';
-import { useKeywords } from '../../hooks';
+import { useSelector } from 'react-redux';
+import { Text, Empty } from '../../components/element';
 
-import { NavBar, ProgressBar1, MainContainer, QuestionContainer } from '..';
+import { QuestionContainer } from '..';
 import style from './index.module.css';
 
-function BlankPage() {
-  const { korean } = useSelector(state => state.game.datasets[state.game.stage - 1]);
+function BlankPage({ LifeState }) {
+  const { korean, english, blankIndex, clause } = useSelector(state => state.game.datasets[state.game.stage - 1]);
   const { stage } = useSelector(state => state.game);
-  const { isCrtAns, TimerUI, LifeState } = useKeywords();
+  const [sentence, setSentence] = React.useState();
+  const engSplit = english.split(' '); //english 띄어쓰기 기준으로 나눈 배열
+
+  //sentence에 빈칸 뚫기
+  const createSentence = () => {
+    let _sentence = '';
+    for (let i = 0; i < clause; i++) {
+      //임시로 해놨는데 i === blankIndex으로 수정
+      if (i === blankIndex + 2) {
+        _sentence += ' _______ ';
+      } else {
+        _sentence += engSplit[i] + ' ';
+      }
+    }
+    return _sentence;
+  };
+
+  //sentence useState에 넣기
+  React.useLayoutEffect(() => {
+    setSentence(() => createSentence());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [stage]);
+
+  console.log(sentence);
 
   return (
     <>
-      <div className={style.container}>
-        <ProgressBar1 value={(stage - 1) * 10} />
-        <TimerUI />
-        <div className={style.relative}>
-          <QuestionContainer content={korean} />
-        </div>
-        <LifeState />
-      </div>
+      <QuestionContainer content={korean} />
+      <Text size="M" content={sentence} />
+      <LifeState />
     </>
   );
 }
--- src/pages/PlayGame/index.jsx
+++ src/pages/PlayGame/index.jsx
@@ -4,12 +4,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { gameSlice } from '../../store/slices';
 import { useKeywords } from '../../hooks';
 
-import { NavBar, MainContainer, ClickPage, BlankPage } from '../../components';
+import { NavBar, MainContainer, ProgressBar1, ClickPage, BlankPage } from '../../components';
 import { Empty, GradingButton } from '../../components/element';
+import style from './index.module.css';
 
 function PlayGame() {
   const location = useLocation();
   const dispatch = useDispatch();
+  const { korean, english, questionType } = useSelector(state => state.game.datasets[state.game.stage - 1]);
   const { stage, life } = useSelector(state => state.game);
   const { keywords, onNextStage, handleGameOver, isGrading, PointEarnedUI, ShowModal, setKeywords, newKeywords, createKeywordsId, insertButton, removeButton, isCrtAns, TimerUI, LifeState } =
     useKeywords();
@@ -28,23 +30,30 @@ function PlayGame() {
     }
   }, [location, dispatch]);
 
+  //console.log(stage);
+  //console.log(questionType);
+
   return (
     <>
       <NavBar />
       <ShowModal />
       <MainContainer>
         <article>
-          <ClickPage
-            keywords={keywords}
-            setKeywords={setKeywords}
-            newKeywords={newKeywords}
-            createKeywordsId={createKeywordsId}
-            insertButton={insertButton}
-            removeButton={removeButton}
-            isCrtAns={isCrtAns}
-            TimerUI={TimerUI}
-            LifeState={LifeState}
-          />
+          <div className={style.container}>
+            <ProgressBar1 value={(stage - 1) * 10} />
+            <TimerUI />
+            <BlankPage
+              keywords={keywords}
+              setKeywords={setKeywords}
+              newKeywords={newKeywords}
+              createKeywordsId={createKeywordsId}
+              insertButton={insertButton}
+              removeButton={removeButton}
+              isCrtAns={isCrtAns}
+              TimerUI={TimerUI}
+              LifeState={LifeState}
+            />
+          </div>
           <PointEarnedUI />
           <GradingButton content="정답 확인하기" isDisabled={keywords.length > 0 || isGrading} onClick={life >= 1 ? onNextStage : null} />
           <Empty size="1rem" />
