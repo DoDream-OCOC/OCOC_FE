b707a3c042ab57924754c37b6f0a77a6d71cb993
jungeol-h <dali2728@gmail.com>
2022-11-13 15:30:12 +0900

    마이페이지 랭킹 보여주기

--- src/pages/Mypage/cards/Ranking.jsx
+++ src/pages/Mypage/cards/Ranking.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Text, Empty, Button } from '../../../components/element';
 import style from './index.module.css';
 
-export const Ranking = () => {
+export const Ranking = ({ rank, id, score }) => {
   return (
     <section>
-      <Empty size="4rem" />
-      <div className={style.title_wrapper}>
-        <Text size="H2" content="랭킹" />
+      <Empty size="0.5rem" />
+      <div className={rank === 'MY' ? style.title_wrapper : style.title_wrapper_other}>
+        <Text color={rank === 'MY' ? 'Text-3' : 'Text-2'} size="B1" content={rank} />
+        <Text color={rank === 'MY' ? 'Text-3' : 'Text-2'} size="B1" content={id} />
+        <Text color={rank === 'MY' ? 'Text-3' : 'Text-2'} size="B1" content={score} />
       </div>
     </section>
   );
