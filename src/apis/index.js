5312e0413b3b2d486bd36bb4407ec66aa4b380cd
Jchan <gkswocks123@gmail.com>
2022-11-13 13:23:00 +0900

    Feat : Create postScore api

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,5 +1,6 @@
 import * as study from './study';
 import * as question from './question';
 import * as sign from './sign';
+import * as score from './score';
 
-export { study, question, sign };
+export { study, question, sign, score };
