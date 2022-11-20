33519ba82ab36324619adf492e9205e8712eaa9e
jungeol-h <dali2728@gmail.com>
2022-11-21 02:56:59 +0900

    스테이지 넘어가면 배경 색 변경 구현

--- src/components/background/index.jsx
+++ src/components/background/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Lottie from 'lottie-react';
 import cloud_bg from '../../assets/OCOC/Cloud_background.json';
 import style from './index.module.css';
-function Background() {
+function Background({ color }) {
   return (
-    <div className={style.background_wrapper}>
+    <div className={style.background_wrapper} style={{ background: `linear-gradient(${color}, white)` }}>
       <Lottie animationData={cloud_bg} />
     </div>
   );
