338099259e3ad2e5ce1234597ebcca9e4b2021e4
jungeol-h <dali2728@gmail.com>
2022-11-21 03:31:11 +0900

    Sign wrapper 추가

--- src/pages/sign/SignUp/index.jsx
+++ src/pages/sign/SignUp/index.jsx
@@ -4,6 +4,7 @@ import { useSignUp } from './useSignUp';
 import NavBar from '../../../components/navbar';
 import MainContainer from '../../../components/container/main';
 import { Text, Button, Empty, Input } from '../../../components/element';
+import style from '../../../pages/sign/index.module.css';
 
 const SIGN_UP = 'sign-up';
 
@@ -15,17 +16,19 @@ function SignUp() {
       <NavBar />
       <MainContainer>
         <article>
-          <Text size="H4" color="Text-2" content="회원가입" />
-          <Empty size="2.5rem" />
-          <form id={SIGN_UP} onSubmit={onSubmit}>
-            <Input {...reg.email} vldErr={vldErr.email} type="email" placeholder="Email" />
-            <Empty size="2rem" />
-            <Input {...reg.password} vldErr={vldErr.password} type="password" placeholder="비밀번호" />
-            <Empty size="2rem" />
-            <Input {...reg.confirmPassword} vldErr={vldErr.confirmPassword} type="password" placeholder="비밀번호 재입력" />
-          </form>
-          <Empty size="6rem" />
-          <Button content="회원가입" type="submit" form={SIGN_UP} />
+          <section className={style.sign_wrapper}>
+            <Text size="H4" color="Text-2" content="회원가입" />
+            <Empty size="2.5rem" />
+            <form id={SIGN_UP} onSubmit={onSubmit}>
+              <Input {...reg.email} vldErr={vldErr.email} type="email" placeholder="Email" />
+              <Empty size="2rem" />
+              <Input {...reg.password} vldErr={vldErr.password} type="password" placeholder="비밀번호" />
+              <Empty size="2rem" />
+              <Input {...reg.confirmPassword} vldErr={vldErr.confirmPassword} type="password" placeholder="비밀번호 재입력" />
+            </form>
+            <Empty size="6rem" />
+            <Button content="회원가입" type="submit" form={SIGN_UP} />
+          </section>
         </article>
       </MainContainer>
     </>
