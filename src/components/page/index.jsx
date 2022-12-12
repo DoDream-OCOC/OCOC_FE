4d101e6b4410834d48c8f160682c719b6d55b7bd
jungeol-h <dali2728@gmail.com>
2022-12-13 06:59:38 +0900

    빈칸영작 미동작 에러 해결 빠져있는 인자들을 추가했더니 정상 동작합니다 CSS inline으로 빠르게 수정했습니다

--- src/components/page/index.jsx
+++ src/components/page/index.jsx
@@ -2,7 +2,23 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { ClickPage, BlankPage } from '../../components';
 
-function Page({ keywords, setKeywords, newKeywords, createKeywordsId, insertButton, removeButton, isCrtAns, LifeState, blankText, onChange, CrtAnswerUI, fadeInOut }) {
+function Page({
+  keywords,
+  setKeywords,
+  newKeywords,
+  createKeywordsId,
+  insertButton,
+  removeButton,
+  isCrtAns,
+  LifeState,
+  blankText,
+  onChange,
+  CrtAnswerUI,
+  fadeInOut,
+  sentences,
+  setSentences,
+  createSentence,
+}) {
   const { questionType } = useSelector(state => state.game.datasets[state.game.stage - 1]);
 
   if (questionType === 'CLICK') {
@@ -27,11 +43,14 @@ function Page({ keywords, setKeywords, newKeywords, createKeywordsId, insertButt
       <>
         <BlankPage
           keywords={keywords}
+          sentences={sentences}
           setKeywords={setKeywords}
           newKeywords={newKeywords}
           createKeywordsId={createKeywordsId}
           insertButton={insertButton}
           removeButton={removeButton}
+          createSentence={createSentence}
+          setSentences={setSentences}
           isCrtAns={isCrtAns}
           LifeState={LifeState}
           blankText={blankText}
