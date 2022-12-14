b35cee4df1482f2e1dc2563f2b4c0b5a55237c90
Jchan <gkswocks123@gmail.com>
2022-12-14 15:44:27 +0900

    Feat : 리프레쉬 토큰 재발급 로직 부착

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -1,10 +1,16 @@
 import ococ from './core';
+import store from '../store';
+import { signSlice } from '../store/slices';
 
 const ROUTE = 'auth';
 
-export const postRefreshToken = async ({ refreshToken }) => {
-  await ococ.post(`/${ROUTE}/refresh`, { headers: { refreshToken } }).then(res => {
-    console.log(res);
+export const postRefreshToken = async () => {
+  await ococ.post(`/${ROUTE}/refresh`).then(res => {
+    store.dispatch(
+      signSlice.actions.setAccessToken({
+        X_AUTH_ACCESS_TOKEN: res.data.data.accessToken,
+      }),
+    );
     return res;
   });
 };
--- src/pages/Test/index.jsx
+++ src/pages/Test/index.jsx
@@ -3,35 +3,26 @@ import { useNavigate } from 'react-router-dom';
 import NavBar from '../../components/navbar';
 import MainContainer from '../../components/container/main';
 
-import { score } from '../../apis';
-import { Text, Empty, Button } from '../../components/element';
-import ChartModal from '../Mypage/modal';
-import { useModal } from '../../hooks';
+import { auth } from '../../apis';
+import { Text, Button } from '../../components/element';
 
 function Test() {
-  // [Todo] 브라우저 탭 돌리면 멈추는 거 해결하기
   const navigate = useNavigate();
-  const { Modal, closeModal, openModal } = useModal();
 
   const testPost = async () => {
-    const res = await score.postScore(30, 3000, 1290);
-    console.log(res);
-    openModal();
+    const res = await auth.postRefreshToken();
   };
 
   return (
     <>
       <NavBar />
-      <Modal>
-        <ChartModal closeModal={closeModal} />
-      </Modal>
       <MainContainer>
         <article>
           <Text size="H3" color="black" content="테스트 페이지입니다." style={{ wordSpacing: '-3px' }} />
           <div onClick={() => navigate('/')}>
             <Text size="B1" content="홈페이지로 돌아가기" />
           </div>
-          <Button onClick={testPost} content="postScore" />
+          <Button onClick={testPost} content="reissueToken" />
         </article>
       </MainContainer>
     </>
